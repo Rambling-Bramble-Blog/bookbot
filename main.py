d1437e124d4f7501a992de198680d32ea52e3d35
Rambling-Bramble-Blog <rambling.bramble.blog@gmail.com>
2026-01-04 22:27:22 -0800

    import sys and adjust main to accept sys.argv[1] for book path

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words,  get_num_char, sort_char_dict
 
 def get_book_text(file_path):
@@ -6,15 +7,18 @@ def get_book_text(file_path):
         return file_contents
     
 def main():
-    frankenstein_rel_path = "books/frankenstein.txt"
     
-    book_text = get_book_text(frankenstein_rel_path)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_text = get_book_text(sys.argv[1])
     num_words = get_num_words(book_text)    
     num_char_dict = get_num_char(book_text)
     sorted_char_dict = sort_char_dict(num_char_dict)
 
     
-    print(f"============ BOOKBOT ============\nAnalyzing book found at {frankenstein_rel_path}...\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------")
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {sys.argv[1]}...\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------")
     for i in sorted_char_dict:
         if i["char"].isalpha():
             print(f"{i['char']}: {i['num']}")
